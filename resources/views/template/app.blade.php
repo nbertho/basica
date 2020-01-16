2346e7d857dfddf6c80ec2bbee358ce028bee2e2
Nicolas B <n.bertho.web@gmail.com>
2020-01-16 14:36:36 +0100

    Intégration du template, des partials et de la zone dynamique
    - Intégration du template, des partials et de la zone dynamique
    - Modification temporaire de la route par défaut afin de vérifier l'intégration du template

---
+++ resources/views/template/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="en">
+
+  <head>
+    @include('template.partials.head')
+  </head>
+
+  <body>
+
+    <!--[if lt IE 7]>
+      <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- Header -->
+      @include('template.partials.header')
+
+    <!-- Main content -->
+      @section('contenu')
+      @show
+
+    <!-- Footer -->
+  	  @include('template.partials.footer')
+
+    <!-- Scripts -->
+  	  @include('template.partials.scripts')
+
+  </body>
+</html>
---
+++ resources/views/template/partials/scripts.blade.php
@@ -0,0 +1,12 @@
+{{--
+  templates/partials/scripts
+--}}
+
+<!-- Javascripts -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="{{ asset('js/jquery-1.9.1.min.js') }}"><\/script>')</script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+<!-- Scrolling Nav JavaScript -->
+  <script src="{{ asset('js/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('js/scrolling-nav.js') }}"></script>
